cd8b86e64b3ddd98c0302c7e2b881285ff10d49b
P1M <renato.barbosa.pim.pereira@hotmail.com>
2018-01-28 00:02:37 -0300

    banco de dados

--- src/test.java
+++ src/test.java
@@ -1,2 +1,24 @@
+import java.sql.*;
+
+
 public class test {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        // Forço a instanciação da classe Driver da implementação fornecida pela MySQL
+        // Carregar o Driver (ETAPA 2)
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        // Especifica uma URL com informações para acessar o Banco de Dados (ETAPA 3)
+        String url = "jdbc:mysql://localhost:3306/"; // Nome da base de dados
+        String user = "root"; // nome do usuário do MySQL
+        String password = "123pimpim"; // senha do MySQL
+
+        // Abre uma conexão com o Banco de Dados (ETAPA 4)
+        Connection c = DriverManager.getConnection(url, user, password);
+
+
+        c.close();
+    }
+
+
 }
